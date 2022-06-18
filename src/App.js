1d302cba9345040ea7766f5cb4e8312f4052822a
noyflaysher <noyflaysher12@gmail.com>
2022-06-18 16:49:14 +0300

    mealItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Layout/Header";
 import React from "react";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
