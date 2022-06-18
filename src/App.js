bc642fc72adf723dee111e0a2531c45725effca0
noyflaysher <noyflaysher12@gmail.com>
2022-06-18 17:47:58 +0300

    modal component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import { Fragment } from "react";
+
 import Header from "./components/Layout/Header";
-import React from "react";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
-    <>
+    <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
       </main>
-    </>
+    </Fragment>
   );
 }
 
